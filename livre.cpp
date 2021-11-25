b01c1b1f666c036e582a4bb96f78e47d864b6b2b
MaxBonnici <95021980+MaxBonnici@users.noreply.github.com>
2021-11-25 10:45:47 +0100

    Question 2 - Livre

---
+++ livre.cpp
@@ -0,0 +1,27 @@
+
+
+#include "livre.h"
+
+std::string Livre::title() const {
+	return _title;
+}
+
+std::string Livre::language() const {
+	return _language;
+}
+
+std::string Livre::genre() const {
+	return _genre;
+}
+
+std::string Livre::auteur() const{
+	return _auteur;
+}
+
+std::string Date::toString(a_day) const;
+
+int Livre::ISBN() const{
+	return _ISBN;
+}
+
+
---
+++ livre.h
@@ -0,0 +1,21 @@
+
+
+ #include "auteur.h"
+ #include "date.h"
+
+class Livre {
+ public:
+	std::string title() const;
+	std::string language() const;
+	std::string genre() const;
+	std::string auteur() const;
+	int ISBN() const;
+
+private:
+
+};
+
+}
+
+	
+	
