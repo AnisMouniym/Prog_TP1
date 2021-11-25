804da1fc9b2ed444c4e179f45a154062e724c78f
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2021-11-25 10:12:53 +0100

    Question1 - Class Date

---
+++ date.h
@@ -0,0 +1,26 @@
+#ifndef DATE_H
+#define DATE_H
+#include <string>
+
+class Date {
+public:
+	Date(int year=1, int month=1, int day=1);
+	int year() const;
+	int month() const;
+	int day() const;
+	void updateMonth(int month);
+	void updateDay(int day);
+	void next();
+	void back();
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+bool isDate(int month, int day);
+int getDaysInMonth(int month);
+int dayOfYear(Date d);
+std::string toString(Date d);
+
+#endif // DATE_H
---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+#include "date.h"
+#include <iostream>
+
+int main() {
+  Date a_day(2000,12,31);
+  std::cout << "a day: " << toString(a_day) << std::endl;
+  std::cout << "day #" << dayOfYear(a_day) << std::endl;
+  a_day.next();
+  std::cout << "a day++: " << toString(a_day) << std::endl;
+  a_day.back();
+  std::cout << "a day: " << toString(a_day) << std::endl;
+  return 0;
+}
