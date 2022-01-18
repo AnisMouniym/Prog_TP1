5ab68b95fc523d46519ed53caeaa1fb17b2e6dcc
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2022-01-18 10:59:48 +0100

    Update livre.cpp

--- livre.cpp
+++ livre.cpp
@@ -3,10 +3,12 @@
 #include "livre.h"
 #include "date.h"
 #include <assert.h>
-#include <vector>
+#include "auteur.h"
+#include "date.h"
+#include <iostream>
 #include <string>
 
-Livre::Livre(std::string title, std::string language, std::string genre, std::string auteur, std::string ISBN, Date date) : _title(title), _language(language), _genre(genre), _auteur(auteur), _ISBN(ISBN), _date(date) {}
+Livre::Livre(std::string title, std::string language, std::string genre, Auteur auteur, std::string ISBN, Date date) : _title(title), _language(language), _genre(genre), _auteur(auteur), _ISBN(ISBN), _date(date) {}
 
 
 std::string Livre::title() const {
@@ -21,18 +23,17 @@ std::string Livre::genre() const {
 	return _genre;
 }
 
-std::string Livre::auteur(Auteur a) const {
-	return a.getFullName();
-}
-
-std::string date(Date d) const {
-	return toString(d);
+std::string Livre::auteur() const {
+	return getFullName(_auteur);
 }
 
 std::string Livre::ISBN() const {
 	return _ISBN;
 }
 
+std::string Livre::date() const {
+	return toString(_date);
+}
 
 
 
