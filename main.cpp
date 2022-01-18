fe83b62b9f0325eb20993c410d7ca4414d249bcb
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2022-01-18 11:32:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 
 int main() {
+// test de la classe Date
 	Date a_day(2000, 3,1);
 	std::cout << "a day: " << toString(a_day) << std::endl;
 	std::cout << "Jour n°" << dayOfYear(a_day) << " de l'année" << std::endl;
@@ -12,13 +13,17 @@ int main() {
 	std::cout << "a day++: " << toString(a_day) << std::endl;
 	a_day.back();
 	std::cout << "a day: " << toString(a_day) << std::endl;
-
+// test de la classe Lecteur
 	Lecteur l("troth", "Tom", "Roth");
 	std::cout << getFullName(l) << " ID: " << l.id() << std::endl;
-
+// test de la classe Auteur
 	Auteur a("J.K", "Rowling");
 	std::cout << getFullName(a) << std::endl;
-	
+// test de la classe Livre	
+	Livre livre1("Harry Potter", "French", "Roman", a, "0179279258",a_day);
+	Livre livre2("Harry Potter 2", "French", "Roman", a, "0179279259",a_day);
+	std::cout << livre1.auteur() << ", " << livre1.title() << std::endl;
+// test de la classe Emprunt
 	
 	return 0;
 }
