31ee3c6a97afdb8e214508a27c81ad382f3f7858
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2022-01-18 11:04:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,24 @@
 #include "date.h"
 #include "Lecteur.h"
 #include "auteur.h"
+#include "livre.h"
 #include <iostream>
 
 int main() {
-	Date a_day(2000, 2, 29);
+	Date a_day(2000, 3,1);
 	std::cout << "a day: " << toString(a_day) << std::endl;
-	std::cout << "day # " << dayOfYear(a_day) << std::endl;
+	std::cout << "Jour n°" << dayOfYear(a_day) << " de l'année" << std::endl;
 	a_day.next();
 	std::cout << "a day++: " << toString(a_day) << std::endl;
 	a_day.back();
 	std::cout << "a day: " << toString(a_day) << std::endl;
 
-	Lecteur l("bg", "Tom", "Roth");
-	std::cout << getFullName(l) << " le " << l.id() << std::endl;
+	Lecteur l("troth", "Tom", "Roth");
+	std::cout << getFullName(l) << " ID: " << l.id() << std::endl;
 
-	Auteur a("Adolf", "H");
+	Auteur a("J.K", "Rowling");
 	std::cout << getFullName(a) << std::endl;
+	
+	
 	return 0;
 }
